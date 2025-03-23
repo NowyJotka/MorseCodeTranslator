d76189d87bbdd4b66b80c3f74064ddd379c54a88
Nowy Jotka <nowyjtka@outlook.com>
2025-03-23 23:42:55 +0100

    Update Morse.py
    Updated the code, now you can revert morse to text and better

--- Morse.py
+++ Morse.py
@@ -1,41 +1,96 @@
-# Define the Morse code dictionary
-morse_code = {
-    'a': ".-", 
-    'b': "-...", 
-    'c': "-.-.", 
-    'd': "-..", 
-    'e': ".", 
-    'f': "..-.", 
-    'g': "--.", 
-    'h': "....", 
-    'i': "..", 
-    'j': ".---", 
-    'k': "-.-", 
-    'l': ".-..", 
-    'm': "--", 
-    'n': "-.", 
-    'o': "---", 
-    'p': ".--.", 
-    'q': "--.-", 
-    'r': ".-.", 
-    's': "...", 
-    't': "-", 
-    'u': "..-", 
-    'v': "...-", 
-    'w': ".--", 
-    'x': "-..-", 
-    'y': "-.--", 
-    'z': "--.."
-}
+seleccion = input("""Bienvenido al traductor de morse
+      Welcome to the Morse translator
+      
+      Select '1' to text to morse
+      Selecciona '1' para texto a morse
+      
+      And '2' to morse to text
+      Y '2' para morse a texto:
+       """)
 
-traducir = input("What do you want to translate?: ").lower()
+if seleccion == "1":
+    morse_code = {
+        'a': ".-", 
+        'b': "-...", 
+        'c': "-.-.", 
+        'd': "-..", 
+        'e': ".", 
+        'f': "..-.", 
+        'g': "--.", 
+        'h': "....", 
+        'i': "..", 
+        'j': ".---", 
+        'k': "-.-", 
+        'l': ".-..", 
+        'm': "--", 
+        'n': "-.", 
+        'o': "---", 
+        'p': ".--.", 
+        'q': "--.-", 
+        'r': ".-.", 
+        's': "...", 
+        't': "-", 
+        'u': "..-", 
+        'v': "...-", 
+        'w': ".--", 
+        'x': "-..-", 
+        'y': "-.--", 
+        'z': "--.."
+    }
 
-translation = ""
-for char in traducir:
-    if char == " ":
-        translation += "/"  
-    elif char in morse_code:
-        translation += morse_code[char] + " "  
+    traducir = input("""What do you want to translate?: 
+Que quieres traducir?""").lower()
 
-print(translation.strip())
-print(input("Press enter to escape the terminal"))
+    translation = ""
+    for char in traducir:
+        if char == " ":
+            translation += "/"  
+        elif char in morse_code:
+            translation += morse_code[char] + " "  
+
+    print(translation.strip())
+    print(input("""Press enter to escape the terminal
+ Dale a enter para salir de la terminal""")) 
+
+elif seleccion == "2":
+    code_morse = {
+        '.-': 'a',
+        '-...': 'b',
+        '-.-.': 'c',
+        '-..': 'd',
+        '.': 'e',
+        '..-.': 'f',
+        '--.': 'g',
+        '....': 'h',
+        '..': 'i',
+        '.---': 'j',
+        '-.-': 'k',
+        '.-..': 'l',
+        '--': 'm',
+        '-.': 'n',
+        '---': 'o',
+        '.--.': 'p',
+        '--.-': 'q',
+        '.-.': 'r',
+        '...': 's',
+        '-': 't',
+        '..-': 'u',
+        '...-': 'v',
+        '.--': 'w',
+        '-..-': 'x',
+        '-.--': 'y',
+        '--..': 'z'
+    }
+
+    revertir = input("""What do you want to convert?
+Que quieres convertir?""")
+    reverticion = ""
+    for char in revertir.split():
+        if char == "/":
+            reverticion += " "  
+        elif char in code_morse:
+            reverticion += code_morse[char]  
+
+    print(reverticion.strip())
+    print(input("""Press enter to exit
+Dale a enter para salir"""))
